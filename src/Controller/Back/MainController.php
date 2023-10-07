648f1e0609a30874c8711bb4535a2bdb3f9e6fa8
mika rihani <mikabernik@gamil.com>
2023-10-07 14:44:37 +0200

    fix mainController corretcion de la 404 sur la home page

--- src/Controller/Back/MainController.php
+++ src/Controller/Back/MainController.php
@@ -13,7 +13,7 @@ class MainController extends AbstractController
     /**
      * route to home page with data 
      * 
-     * @Route("/admin", name="app_back_main_home", methods={"GET"})
+     * @Route("/", name="app_back_main_home", methods={"GET"})
      *
      * @param GardenRepository $gardenRepository
      * @param UserRepository $userRepository
